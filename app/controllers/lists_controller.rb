52d404c8f355f517dc6bb8c6dc5321e43e3dd39e
Chadwick Bidwell + Damien Kan <apprentice+cb+dk@devbootcamp.com>
2012-08-03 15:56:36 -0700

    ajaxified a few things.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,17 +3,25 @@ class ListsController < ApplicationController
   def new
     @project = Project.find(params[:project_id])
     @list = @project.lists.new
+    respond_to do |format|
+      format.js {render :action => 'new'}
+    end
   end
 
   def create
     @list = List.new(params[:list])
-    if @list.save
-      flash[:message] = "Good job creating a list."
-      redirect_to project_path(@list.project_id)
-    else
-      flash[:message] = "Bad job."
-      render :new
+    @project = Project.find(@list.project_id)
+    respond_to do |format|
+      if @list.save
+        flash[:message] = "Good job creating a list."
+        # redirect_to project_path(@list.project_id)
+        format.js {render :action => 'restore'}
+      else
+        flash[:message] = "Bad job."
+        render :new
+      end
     end
+
   end
 
   def show
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,28 @@
 class TasksController < ApplicationController
 
+  def new
+    @list = List.find(params[:id])
+    @task = @list.tasks.new
+    respond_to do |format|
+      format.js {render :action => 'new'}
+    end
+  end
+
   def create
     @task = Task.new(params[:task])
+    @list = @task.list
+    # @list = List.find(params[:task][:list_id])
+    respond_to do |format|
+
       if @task.save
         flash[:success] = "More work created!"
-        redirect_to project_path(@task.list.project_id)
+        # redirect_to project_path(@task.list.project_id)
+        format.js {render :action => 'restore'}
       else
         flash[:error] = "No go!"
         render 'list/show'
       end
+    end
   end
 
 
